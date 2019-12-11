4515d9b07680acf8212b0b5247a4db0803adc41c
Kashish <kashigarg1111@gmail.com>
2019-12-11 17:42:06 +0530

    First commit

---
+++ 822A.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int main()
+{
+	long long int a,b,ans=1;
+	cin >> a >> b;
+	for(int i=1; i<=min(a,b); i++)
+		ans*=i;
+	cout << ans << endl;
+	return 0;
+} 
