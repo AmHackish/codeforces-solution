774fbb837c6137e5ba8f1c42469518677d868c99
Kashish <kashigarg1111@gmail.com>
2019-12-16 17:10:14 +0530

    commit

---
+++ 1096A.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int main()
+{
+	int t;
+	cin >> t;
+	for(int i=0; i<t; i++)
+	{
+		long long int l,r,x,y;
+		cin >> l >> r;
+		cout << l << " " <<  l*2;
+		cout << endl;
+	}
+	return 0;
+}  
