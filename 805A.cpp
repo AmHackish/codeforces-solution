fba8434463390974957e09b6a251ccd7e9d540b2
Kashish <kashigarg1111@gmail.com>
2019-12-18 20:01:34 +0530

    commit

---
+++ 805A.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	long long int l,r;
+	std::vector<int> v;
+	cin >> l >> r;
+	if(l == r)
+		cout << l << endl;
+	else 
+		cout << 2 << endl;
+	return 0;
+}  
