f78590567bd85f1ffa137b4b33f10881719a2a16
Kashish <kashigarg1111@gmail.com>
2019-12-20 19:33:33 +0530

    commit

--- 155A.cpp
+++ 155A.cpp
@@ -1,11 +1,27 @@
 #include <bits/stdc++.h>
- 
 using namespace std;
 
 int main()
 {
 	int n;
 	cin >> n;
-	cout << "25" << endl;
+	std::vector<int> v(n);
+	for(int i=0;i<n;i++)
+		cin >> v[i];
+	int mini = v[0],maxi=v[0],cnt=0;
+	for(int i=1;i<n;i++)
+	{
+		if(v[i]<mini)
+		{
+			cnt++;
+			mini=v[i];
+		}
+		else if(v[i]>maxi)
+		{
+			cnt++;
+			maxi = v[i];
+		}
+	}
+	cout << cnt << endl;
 	return 0;
-}
+}  
