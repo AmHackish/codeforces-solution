4d708b923ff90d71e18c519fa681e97618594f0c
Kashish <kashigarg1111@gmail.com>
2020-01-22 18:24:04 +0530

    Commit

---
+++ 914A.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+int main()
+{
+	int n;
+	cin >> n;
+	vector<int>a(n);
+	for(int i=0;i<n;i++)
+		cin >> a[i];
+	sort(a.begin(),a.end());
+	for(int i=n-1;i>=0;i--)
+	{
+		int y = sqrt(a[i]);
+		float z = sqrt(a[i]);
+		if(y!=z)
+		{
+			cout << a[i] << endl;
+			return 0;
+		}
+	}
+	return 0;
+}
