3a9dd5181374435c8cced46eadf144c602b04b4d
Kashish <kashigarg1111@gmail.com>
2020-01-27 19:36:37 +0530

    commit

--- 757A.cpp
+++ 757A.cpp
@@ -10,30 +10,22 @@ int main()
 	for(int i=0;i<s.length();i++)
 	{
 		if(s[i]=='B')
-			v[0]++;
+			v[0]+=2;
 		if(s[i]=='u')
 			v[1]++;
 		if(s[i]=='l')
-			v[2]++;
+			v[2]+=2;
 		if(s[i]=='b')
-			v[3]++;
+			v[3]+=2;
 		if(s[i]=='a')
 			v[4]++;
 		if(s[i]=='s')
-			v[5]++;
+			v[5]+=2;
 		if(s[i]=='r')
-			v[6]++;
+			v[6]+=2;
 	}
 	int cnt=0;
-	cnt = min(v[0],min(v[2],min(v[6],min(v[3],v[5]))));
-	if(v[1]<=1  or v[4]<=1)
-		cout << 0 << endl;
-	else
-	{
-		v[1]/=2;
-		v[4]/=2;
-		int cnt1 = min(v[1],v[4]);
-		cout << min(cnt1,cnt1) << endl;
-	}
+	sort(v.begin(),v.end());
+	cout << v[0]/2 << endl;
 	return 0;
 }
