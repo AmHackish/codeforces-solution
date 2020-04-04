ac429180220f6909d15f7637ebba994acfcc2eb6
Kashish <kashigarg1111@gmail.com>
2020-04-04 18:59:49 +0530

    Remove a Progression

---
+++ 1194A.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int lli;
+
+int main()
+{
+	int t;
+	cin >> t;
+
+	while(t--)
+	{
+		long long int n,x;
+		cin >> n >> x;
+		cout << 2*x << endl;
+	}
+	return 0;
+}
