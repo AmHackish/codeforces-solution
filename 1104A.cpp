31d45412473b627bf0eb60191bd7acda3d102640
Kashish <kashigarg1111@gmail.com>
2020-04-04 18:49:04 +0530

    Splitting into digits

---
+++ 1104A.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int lli;
+
+int main()
+{
+	int n;
+	cin >> n;
+
+	cout << n << endl;
+	for(int i=0;i<n;i++)
+		cout << 1 << " ";
+	cout << endl;
+	return 0;
+}
